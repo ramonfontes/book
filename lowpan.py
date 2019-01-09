294da3e334f1695b29a07f71524001f436517f9d
Ramon Fontes <ramonreisfontes@gmail.com>
2019-01-08 23:20:48 -0300

    update code

--- lowpan.py
+++ lowpan.py
@@ -13,17 +13,17 @@ def topology():
     net = Mininet_wifi()
 
     info("*** Creating nodes\n")
-    node1 = net.add6LoWPAN('node1', ip='2001::1/64')
-    node2 = net.add6LoWPAN('node2', ip='2001::2/64')
-    node3 = net.add6LoWPAN('node3', ip='2001::3/64')
+    sta1 = net.add6LoWPAN('sta1', ip='2001::1/64')
+    sta2 = net.add6LoWPAN('sta2', ip='2001::2/64')
+    sta3 = net.add6LoWPAN('sta3', ip='2001::3/64')
 
     info("*** Configuring nodes\n")
     net.configureWifiNodes()
 
     info("*** Associating Nodes\n")
-    net.addLink(node1, cls=sixLoWPANLink, panid='0xbeef')
-    net.addLink(node2, cls=sixLoWPANLink, panid='0xbeef')
-    net.addLink(node3, cls=sixLoWPANLink, panid='0xbeef')
+    net.addLink(sta1, cls=sixLoWPANLink, panid='0xbeef')
+    net.addLink(sta2, cls=sixLoWPANLink, panid='0xbeef')
+    net.addLink(sta3, cls=sixLoWPANLink, panid='0xbeef')
 
     info("*** Starting network\n")
     net.build()
